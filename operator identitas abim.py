e4f0c4f1778483c265762d7fecdeff8963108084
abimriyansah22 <abimriyansah22@gmail.com>
2024-10-11 10:03:08 +0700

    Add files via upload

---
+++ operator identitas abim.py
@@ -0,0 +1,12 @@
+a = 5
+b = 5
+nama_a = 'Abim'
+nama_b = 'Abim'
+
+# output true
+print('a is b:', a is not b)
+
+#output False
+print('a is not not b:', nama_a is nama_b)
+#output False
+print('name_a is not nama_b:', nama_a is not nama_b)
---
+++ operator keanggotaan abim.py
@@ -0,0 +1,25 @@
+perusahaan = 'microsoft'
+lisr_pulau = ['Jawa', 'Sumatra', 'Sulawesi']
+
+mahasiswa = {
+    'nama': 'Abim riyansah',
+    'asal': 'Pagarawan'
+}
+
+print(
+    "Apakah 'c' ada di variabel perusahaan?",
+    'c' in perusahaan
+)
+print(
+    "Apakah 'z' tidak ada di variabel perusahaan?",
+    'c' not in perusahaan
+)
+print(
+    "Apakah 'Madura' tidak ada di variabel list_pulau",
+    'Madura' not in perusahaan
+)
+print(
+    "Apabila atribut 'nama' ada di variabel mahasiswa",
+    'nama' in mahasiswa
+    
+)
